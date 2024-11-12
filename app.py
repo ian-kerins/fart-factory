2eb84c8e89894660c3c6bf404874b63877e9f91b
ian-kerins <hello@iankerins.com>
2024-11-12 17:46:14 +0100

    fix_sounds

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# Make sure all your routes return static-friendly content
-@app.route('/')
-def index():
-    return render_template('index.html')
+# # Make sure all your routes return static-friendly content
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-# Add other routes...
+# # Add other routes...
 
-# Add this if your site is not at the root of the domain
-app.config['FREEZER_RELATIVE_URLS'] = True
-app.config['FREEZER_DESTINATION'] = 'build'
+# # Add this if your site is not at the root of the domain
+# app.config['FREEZER_RELATIVE_URLS'] = True
+# app.config['FREEZER_DESTINATION'] = 'build'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
