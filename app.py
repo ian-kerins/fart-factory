b2cdad3665323c0fc8aa09dd477c31508a28e726
ian-kerins <hello@iankerins.com>
2024-11-12 17:38:07 +0100

    bug fix

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Make sure all your routes return static-friendly content
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+# Add other routes...
+
+# Add this if your site is not at the root of the domain
+app.config['FREEZER_RELATIVE_URLS'] = True
+app.config['FREEZER_DESTINATION'] = 'build'
 
 if __name__ == '__main__':
     app.run(debug=True)
